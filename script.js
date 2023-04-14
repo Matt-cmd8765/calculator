90b64fde1186fd479b2e13dc7bf80706624f2170
Matt Marini <mattmarini8765@gmail.com>
2023-04-14 18:03:04 +1000

    Got add and buttons working. Some style updates

--- script.js
+++ script.js
@@ -31,16 +31,149 @@ function operate (a,b,op) {
 }
 
 // Test of operations
-let num1 = 12
-let num2 = 6
-let sign = '/';
+// let num1 = 12;
+// let num2 = 6;
+// let sign = '/';
 
 
-let result = operate(num1, num2, sign);
-console.log(result);
+// let result = operate(num1, num2, sign);
+// console.log(result);
 
-const screen = document.getElementById('screen');
-screen.textContent = '899';
+const array = [];
+
+// one button
+const one = document.getElementById('one');
+one.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(1);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const two = document.getElementById('two');
+two.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(2);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const three = document.getElementById('three');
+three.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(3);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const four = document.getElementById('four');
+four.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(4);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const five = document.getElementById('five');
+five.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(5);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const six = document.getElementById('six');
+six.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(6);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const seven = document.getElementById('seven');
+seven.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(7);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const eight = document.getElementById('eight');
+eight.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(8);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const nine = document.getElementById('nine');
+nine.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(9);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const zero = document.getElementById('zero');
+zero.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push(0);
+    let display = array.join('');
+    screen.textContent = display;
+    return parseInt(display);
+});
+
+const plus = document.getElementById('plus');
+plus.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    array.push('+');
+    let display = array.join('');
+    screen.textContent = display;
+});
+
+const equals = document.getElementById('equals');
+equals.addEventListener('click', () => {
+    const screen = document.getElementById('screen');
+    const secondNum = array.findLastIndex((item) => typeof item === "number");
+    const addFinder = array.findIndex(index => index === '+');
+    let numA = parseInt(array.slice(0,addFinder).join(''));
+    let numB = parseInt(array.slice(addFinder, secondNum+1).join(''));
+    console.log(numA);
+    console.log(numB);
+    screen.textContent = add(numA, numB);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
